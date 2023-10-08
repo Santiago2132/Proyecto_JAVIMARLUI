c363cb6763268706fd593f825035369be6591e1b
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-10-08 14:38:35 -0500

    Se eliminaron varios archivos

--- InventoryOdyssey/src/Classes/Administrador.java
+++
@@ -1,6 +0,0 @@
-package Classes;
-
-public class Administrador extends Usuario{
-    //Constructores
-    //Methods
-}
--- InventoryOdyssey/src/Classes/Bodeguero.java
+++
@@ -1,6 +0,0 @@
-package Classes;
-
-public class Bodeguero {
-    //Constructor
-    //Methods
-}

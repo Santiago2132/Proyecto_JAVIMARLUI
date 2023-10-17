c7c343432b882daf3146763617d50e57e8098cd8
Juan Rios <juanjoserios3016@gmail.com>
2023-10-17 15:55:56 -0500

    cambios realizados

--- InventoryOdyssey/src/Controller/ControllerBodeguero.java
+++ InventoryOdyssey/src/Controller/ControllerBodeguero.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import Classes.Inventario;
+import Classes.Producto;
+import Structures.LinkedList;
+
 public class ControllerBodeguero {
+    private Inventario inventario;
+
+    public ControllerBodeguero(Inventario inventario) {
+        this.inventario = inventario;
+    }
+
+    public boolean agregarProducto(Producto producto) {
+        inventario.agregarProducto(producto);
+        return true;
+    }
+
+    public boolean modificarProducto(Producto producto) {
+        inventario.actualizarCantidad(producto.getIdProducto(), producto.getCantidad());
+        inventario.actualizarPrecio(producto.getIdProducto(), producto.getPrecioVenta());
+        return true;
+    }
 }
